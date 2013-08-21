1306a8234a810d3c0409275ede853e10be1fb531
Takuo IMBE <imbe@amoad.com>
2013-08-21 21:11:08 +0900

    done chapter 16

--- src/main/java/Money.java
+++ src/main/java/Money.java
@@ -34,7 +34,7 @@ public class Money implements Expression {
         return amount + " " + currency;
     }
 
-    Expression times(int multiplier) {
+    public Expression times(int multiplier) {
         return new Money(amount * multiplier, currency);
     }
 
--- src/test/java/MoneyTest.java
+++ src/test/java/MoneyTest.java
@@ -1,7 +1,6 @@
 import org.junit.Test;
 
 import static org.hamcrest.CoreMatchers.is;
-import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertThat;
 
 /**
@@ -94,5 +93,27 @@ public class MoneyTest {
         bank.addRate("CHF", "USD", 2);
         Money result = bank.reduce(fiveBucks.plus(tenFrancs), "USD");
         assertThat(result, is(Money.dollar(10)));
+    }
+
+    @Test
+    public void testSumPlusMoney() {
+        Expression fiveBucks = Money.dollar(5);
+        Expression tenFrancs = Money.franc(10);
+        Bank bank = new Bank();
+        bank.addRate("CHF", "USD", 2);
+        Expression sum = new Sum(fiveBucks, tenFrancs).plus(fiveBucks);
+        Money result = bank.reduce(sum, "USD");
+        assertThat(result, is(Money.dollar(15)));
+    }
+
+    @Test
+    public void testSumTimes() {
+        Expression fiveBucks = Money.dollar(5);
+        Expression tenFrancs = Money.franc(10);
+        Bank bank = new Bank();
+        bank.addRate("CHF", "USD", 2);
+        Expression sum = new Sum(fiveBucks, tenFrancs).times(2);
+        Money result = bank.reduce(sum, "USD");
+        assertThat(result, is(Money.dollar(20)));
     }
 }
